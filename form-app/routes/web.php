70d3072cd8b4098cb9e92421ae28a92ad992e963
danindrahmd <danindraahmad@gmail.com>
2023-10-09 16:27:21 +0700

    Update

--- form-app/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FormController;
-
-Route::get('/', [FormController::class, 'index'])->name('form.index');
-Route::post('/submit', [FormController::class, 'submitForm'])->name('form.submit');
-Route::get('/result', [FormController::class, 'showResult'])->name('form.result');
